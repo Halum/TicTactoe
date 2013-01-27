7ec2e946819c9e3ea92c4eb60f59f2e595dffde9
Ilkka Seppälä <iluwatar@gmail.com>
2013-01-27 15:13:37 +0200

    project skeleton
    - wizard generated project with android, desktop and html targets

---
+++ TicTacToe-html/src/com/iluwatar/tictactoe/client/GwtLauncher.java
@@ -0,0 +1,19 @@
+package com.iluwatar.tictactoe.client;
+
+import com.iluwatar.tictactoe.TicTacToeGame;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+
+public class GwtLauncher extends GwtApplication {
+	@Override
+	public GwtApplicationConfiguration getConfig () {
+		GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(480, 320);
+		return cfg;
+	}
+
+	@Override
+	public ApplicationListener getApplicationListener () {
+		return new TicTacToeGame();
+	}
+}
